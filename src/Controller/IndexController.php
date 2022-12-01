97c10c70b4cd8fe390170b71dfca9c9ad8c04536
WojciechPrusaczyk <87181848+WojciechPrusaczyk@users.noreply.github.com>
2022-12-01 16:58:49 +0100

    commit fix

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use http\Env\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(): \Symfony\Component\HttpFoundation\Response
+    {
+        return $this->render('/', [
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/IndexController.php',
+        ]);
+    }
+}

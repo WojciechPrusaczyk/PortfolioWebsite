d5aa769dec0e7bd50fc5b527d38fb1bf4113ad55
WojciechPrusaczyk <87181848+WojciechPrusaczyk@users.noreply.github.com>
2022-12-28 17:56:36 +0100

    Spore zmiany w stylach, oraz strukturze, naprawa responsywnosci

--- assets/js/ReactComponents/index.js
+++ assets/js/ReactComponents/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 function Bar(props) {
     return (
     <div>
-        <p><div className={"nameWIthLogo"}><img src={props.source} alt={props.name + " logo"} className=""/><h3 className={"my-1"}>{props.name}</h3></div></p>
+        <p><div className={"nameWithLogo"}><img src={props.source} alt={props.name + " logo"} className=""/><h3 className={"my-1"}>{props.name}</h3></div></p>
         <div className="progressBar">
             <div className="progress" style={{width: props.progress}}>&nbsp;</div>
         </div>
@@ -31,8 +31,8 @@ class Board extends React.Component {
     }
 
     render() { return (
-            <div className="technologies paragraph flex-row row justify-content-center">
-                <div className="flex-column col-4">
+            <div className="technologies paragraph flex-row row justify-content-around">
+                <div className="flex-column col-xl-3 col-sm-5 col-12">
                     <h2>Programming languages</h2><br/>
                     {this.renderBar("PHP", "/build/images/PHP.png", 70)}
                     <br/><br/>
@@ -43,7 +43,7 @@ class Board extends React.Component {
                     {this.renderBar("VBA", "/build/images/VB.png", 35)}
                     <br/><br/>
                 </div>
-                <div className="flex-column col-4">
+                <div className="flex-column col-xl-3 col-sm-5 col-12 m-sm-0 m-5">
                     <h2>Frameworks and DataBases</h2><br/>
                     {this.renderBar("Symfony", "/build/images/Symfony.png", 70)}
                     <br/><br/>
@@ -57,7 +57,7 @@ class Board extends React.Component {
                     {this.renderBar("Zend", "", 30)}
                     <br/><br/>
                 </div>
-                <div className="flex-column col-4">
+                <div className="flex-column col-xl-3 col-sm-6 col-12 m-sm-0 m-5">
                     <h2>Other tools</h2><br/>
 
                     {this.renderBar("HTML5", "/build/images/HTML5.png", 80)}

5039720a9a15e5d128de44159eb7be61a75242d9
WojciechPrusaczyk <87181848+WojciechPrusaczyk@users.noreply.github.com>
2023-01-03 22:02:43 +0100

    generowanie ostatniego commita

--- assets/js/ReactComponents/index.js
+++ assets/js/ReactComponents/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 let gitHubUsername = "WojciechPrusaczyk";
+let messageLength = 35;
 
 let githubData = {
     status: "Loading...",
@@ -12,6 +13,8 @@ let githubData = {
     work: "Nowhere",
     location: "Nowhere",
     lastUpdate: "2021-07-09 10:32:34",
+    commitDate: "",
+    commitMessage: "",
 }
 
 function Bar(props) {
@@ -92,9 +95,7 @@ class Github extends React.Component {
             return (
                 <div>
                     <img className="profilePicture" src={this.values.imgUrl} alt="profile picture"/>
-                    <p>
-                        <strong>Username: </strong>{this.values.username}
-                    </p>
+                    <h2 className="h4"> {this.values.username} </h2>
                     <p>
                         <strong>Bio: </strong>{this.values.bio}
                     </p>
@@ -102,7 +103,7 @@ class Github extends React.Component {
                         <strong>Github profile link: </strong><a href={this.values.profileLink}>GitHub</a>
                     </p>
                     <p>
-                        <strong>Public repositories count: </strong>{this.values.repos}
+                        <strong>Number of repositories: </strong>{this.values.repos}
                     </p>
                     <p>
                         <strong>Currently working in: </strong>{this.values.work}
@@ -111,7 +112,10 @@ class Github extends React.Component {
                         <strong>Currently living in: </strong>{this.values.location}
                     </p>
                     <p>
-                        <strong>Last account update: </strong>{this.values.lastUpdate}
+                        <strong>Last commit: </strong>{this.values.commitDate}
+                    </p>
+                    <p>
+                        <strong>Last commit: </strong>{this.values.commitMessage}
                     </p>
                 </div>
             )
@@ -140,6 +144,38 @@ technologiesDiv.render(<Technologies />);
         .catch(error => {
             console.error(error);
         });
+    let events = await fetch("https://api.github.com/users/"+gitHubUsername+"/events")
+        .then((response) => response.json())
+        .then(data => {
+            return data;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    let commitMessage = "";
+    let commitDate = "";
+
+    // generowanie ostatniego commita wraz z datą
+    for ( let event of events)
+    {
+        if ("commits" in event['payload'] )
+        for ( let commit of event['payload']['commits'])
+        {
+            if ( commit['author']['name'] === gitHubUsername)
+            {
+                if ( commit['author']['name'] === gitHubUsername && commitDate === "" && commitMessage === "")
+                {
+                    commitDate = event['created_at'].substr(0,10)+" "+event['created_at'].substr(11,8);
+
+                    if (commit['message'].length <= messageLength)
+                        commitMessage = commit['message'];
+                    else
+                        commitMessage = commit['message'].substring(0, messageLength)+"...";
+                    break;
+                }
+            }
+        }
+    }
 
     githubData = {
         status: "Done",
@@ -149,9 +185,11 @@ technologiesDiv.render(<Technologies />);
         profileLink: data['html_url']?data['html_url']:null,
         work: data['company']?data['company']:"Nowhere",
         location: data['location']?data['location']:"Nowhere",
-        lastUpdate: data['updated_at']?data['updated_at'].substr(0,10)+" "+data['updated_at'].substr(11,8):"2021-07-09 10:32:34",
+        commitDate: commitDate,
+        commitMessage: commitMessage,
         repos: repos.length,
     }
+    console.log(githubData);
 
     githubDiv.render(<Github />);
 }();

68b493fb518239dc5e1a617b5a9b77ca84e0eb12
WojciechPrusaczyk <87181848+WojciechPrusaczyk@users.noreply.github.com>
2023-03-07 21:25:48 +0100

    ostylowanie about me

--- assets/js/ReactComponents/aboutMe.js
+++ assets/js/ReactComponents/aboutMe.js
@@ -19,8 +19,8 @@ class AboutMe extends React.Component
     render() {
          return (
              <div className="about-me-component">
-                 <h2>{this.props.name}</h2>
-                 <p dangerouslySetInnerHTML={this.renderAsHtml(this.props.content)} />
+                 <h2 className="about-me-component-header">{this.props.name}</h2>
+                 <p  className="about-me-component-content" dangerouslySetInnerHTML={this.renderAsHtml(this.props.content)} />
              </div>
          );
     }
@@ -46,7 +46,18 @@ class AboutMeRoot extends React.Component{
                     "Through the eight months I made full by myself: " +
                     "Questionnaires system, additional modules to present data on main page and website based on graphic project(<a href='https://marbefes.eu'>marbefes.eu</a>)"
                 )}
-                {this.renderComponent("")}
+                {this.renderComponent("WCAG",
+                    "Through my career in the first company I was working in " +
+                    "I was taught how to make websites accessible for everyone." +
+                    "WCAG is a set of rules to create websites that present it's content in a good way for all users." +
+                    "It's mostly about good html code, font and tools like contrast version of website."
+                )}
+                {this.renderComponent("Hobbies",
+                "In my free time I mostly play video games like From software games, RPG's, MOBA, or FPS's. " +
+                    "I'm always curious about game development process and how they look from the back." +
+                    "I also love swimming which helps me to calm myself." +
+                    "Also Astronomy is my hobby. I just love stars, space and everything around it."
+                )}
             </div>
         );
     }
